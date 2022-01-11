0161570ed9ed8f366e578df65bec090412a891b8
Rob <klassen.rob@gmail.com>
2022-01-11 13:47:20 -0500

    version 1.0

--- scripts.js
+++ scripts.js
@@ -13,26 +13,37 @@ app.buildInputs = function(currentGuess){
     }
 
     const activeInputs = document.querySelectorAll('input');
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    // let alphabet = /^[a-zA-Z]+$/;
+
     activeInputs.forEach(activeInput => {
         activeInput.addEventListener('keydown',function(e){
-            if (e.key === "Backspace"){
-                // this.value = "";
-                const goBackElement = document.querySelector('.checkLetter'+[this.tabIndex - 1]);
-                goBackElement.focus();
-                // console.log("you went back");
-            }
-        })
-        activeInput.oninput = function(e){
-            if (e.inputType === "deleteContentBackward"){
-                null;
-            } else {
-                // console.log(activeInput.value)
-                const thisTabIndex = this.tabIndex;
-                const nextTabIndex = thisTabIndex + 1;
-                const nextInput = document.getElementById('inputID'+[nextTabIndex]);
+            e.preventDefault();
+            const thisTabIndex = this.tabIndex;
+            const nextTabIndex = thisTabIndex + 1;
+            const nextInput = document.getElementById('inputID'+[nextTabIndex]);
+            const goBackElement = document.querySelector('.checkLetter'+[this.tabIndex - 1]);
+
+            if (alphabet.includes(e.key)){
+                this.value = e.key.toUpperCase();
                 nextInput === null ? null : nextInput.focus();
+            } else if (e.key === "Backspace") {
+                if (this.value == ""){
+                    if (goBackElement === null){
+                        console.log("error handling");
+                    } else {
+                        if (((goBackElement.tabIndex + 1) % 5) != 0){
+                            goBackElement.focus();
+                            goBackElement.value = "";
+                        } else {
+                            null;
+                        }
+                    }
+                } else if (((this.tabIndex + 1) % 5) == 0){
+                    this.value = "";
+                } 
             }
-        }        
+        });
     });
 
     //puts focus on new line after pressing enter
@@ -68,12 +79,14 @@ app.checkGuess = function(playerGuessArr, answerAsArr, currentGuess){
 
     const markLetter = function(bool, guessedLetter){
         const letterEl = document.getElementById(`letterID`+guessedLetter);
-        if (bool === true){
-            letterEl.classList.add('letterTrue');
-        }else if (bool === false){
-            letterEl.classList.add('letterFalse');
+        if (guessedLetter === "" || null){
         } else {
-            console.log("lololo");
+            if (bool === true){
+                letterEl.classList.add('letterTrue');
+            }else if (bool === false){
+                letterEl.classList.add('letterFalse');
+            } else {
+            }
         }
     }
 
@@ -83,7 +96,6 @@ app.checkGuess = function(playerGuessArr, answerAsArr, currentGuess){
         for (i=0; i<5; i++){
             if (answerAsArr.includes(playerGuessArr[i])){
                 if (answerAsArr[i] === playerGuessArr[i]){
-                    // console.log(playerGuessArr.indexOf(playerGuessArr[i], i));
                     //make that block green
                     answerGrade.push(2);
 
@@ -129,34 +141,25 @@ app.checkGuess = function(playerGuessArr, answerAsArr, currentGuess){
 
     if (checkArrEquality(playerGuessArr, answerAsArr) == true){
         //VICTORY
-        console.log("victory");
+        app.finalScore();
     } else {
-        //NOT VICTORY
-        console.log("not victory");
         app.buildInputs(currentGuess);
     }
+}
 
+app.finalScore = function(){
+    const overlayEl = document.getElementById('olVictory');
+    overlayEl.classList.remove("hiddenElement");
+    const newGame = document.getElementById('newGame');
+    newGame.addEventListener('click', function(){
+        window.location.reload();
+    });
 }
 
 app.getTargetWord = async function() {
-    // const randomWordGen = function(){   
-        //=================================================================================
-        // let randomWordArr = [];
-        // const alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-        // for (i = 0; i < 5; i++){
-        //     const randNum = Math.ceil((Math.random()*26) - 1);
-        //     const letterToPush = alphabet[randNum];
-        //     randomWordArr.push(letterToPush);
-        // }
-        // return randomWordArr;
-        // // return ["s","a","s","s","y"]  
-        //=================================================================================
-    // }
-    // const answerAsArr = randomWordGen();
-    
     const answerAsArr = await app.randomWordAPI(0);
     const checkButton = document.getElementById("submitGuess");
+    const answerButton = document.getElementById("showAnswer");
     const targetWordTextEl = document.getElementById("wordToGuess");
     targetWordTextEl.innerText = answerAsArr.join("");
     let currentGuess = 0;
@@ -169,30 +172,33 @@ app.getTargetWord = async function() {
 
     checkButton.addEventListener('click', function(){
         executeAction();
-    });  
+    });
+    answerButton.addEventListener('click', function(){
+        targetWordTextEl.style.display = "inline-block";
+        // answerButton.innerText = "SHOW ANSWER";
+    })  
     document.addEventListener('keydown', function(e){
         if (e.code == "Enter"){
             executeAction();
         }
-    })
+    });    
 }
 
 app.buildLetters = function(){
     const letterContainerEl = document.querySelector('.letterContainer');
-    const alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     for (i=0; i<alphabet.length; i++){
         const letterBox = document.createElement('p');
         letterBox.id = 'letterID'+[alphabet[i]];
         letterBox.innerText = alphabet[i];
         letterContainerEl.append(letterBox);
-    }
-    
+    }   
 }
 
 app.randomWordAPI = async function(counter){
     counter = counter + 1;
-    console.log("accessing API for the " + counter + " time");    
+    // console.log("accessing API for the " + counter + " time");    
     const endpointURL = new URL('https://random-word-api.herokuapp.com/word?number=100');
     const myObj = await fetch(endpointURL);
     const jsonData = await myObj.json();
@@ -208,7 +214,7 @@ app.randomWordAPI = async function(counter){
             app.randomWordAPI(counter);
         }
     } else {
-        const randomWordAsArr = fiveLetterWords[0].split("");
+        const randomWordAsArr = fiveLetterWords[0].toUpperCase().split("");
         return randomWordAsArr;
     }
 }
@@ -219,3 +225,79 @@ app.init = function(){
 }
 
 app.init();
+
+
+//======= OLD CODE =======
+// app.getTargetWord = async function() {
+    // const randomWordGen = function(){   
+        // let randomWordArr = [];
+        // const alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+        // for (i = 0; i < 5; i++){
+        //     const randNum = Math.ceil((Math.random()*26) - 1);
+        //     const letterToPush = alphabet[randNum];
+        //     randomWordArr.push(letterToPush);
+        // }
+        // return randomWordArr;
+        // // return ["s","a","s","s","y"]  
+    // }
+    // const answerAsArr = randomWordGen();
+//========================
+                    // console.log(playerGuessArr.indexOf(playerGuessArr[i], i));
+//========================
+
+            // console.log(e);
+            // if (alphabet.includes(e.key)){
+            //     this.value = e.key;
+            // } else {
+            //     // this.value = "";
+            // }
+            // if (e.key === "Backspace"){
+                // e.preventDefault();
+                // this.value = "";
+                // const goBackElement = document.querySelector('.checkLetter'+[this.tabIndex - 1]);
+                // if ((this.tabIndex + 1) % 5 == 0){
+                    // console.log(this.tabIndex, "last element");
+                    // if (this.value == ""){
+                        // console.log("its an empty tile");
+                        // this.value ="";
+                    // } else {
+                        // console.log("it's not an empty tile");
+                        // goBackElement.value = "";
+                        // goBackElement.focus();
+                        //=====================================
+                        // ACTIVE CODE ACTIVE CODE START HERE
+                        //=====================================
+                    // }
+                // } else {
+                    // goBackElement.focus();
+                    // goBackElement.value = "";
+                    // console.log(this.tabIndex, "not last element");
+                // }
+                // console.log("you went back");
+            // }
+
+
+        // activeInput.oninput = function(e){
+        //     if (e.inputType === "deleteContentBackward"){
+        //         null;
+        //     } else {
+        //         if (alphabet.includes(e.data)){
+        //             // console.log(activeInput.value)
+        //             const thisTabIndex = this.tabIndex;
+        //             const nextTabIndex = thisTabIndex + 1;
+        //             const nextInput = document.getElementById('inputID'+[nextTabIndex]);
+        //             // const submitButton = document.getElementById('submitGuess');
+        //             // nextInput === null ? submitButton.focus() : nextInput.focus();
+        //             nextInput === null ? null : nextInput.focus();
+        //         } else {
+        //             // this.value = "";
+                    
+        //         }
+        //     }
+        // }     
+
+
+
+
+//========================
